9e0d73eadd26528420e2e88e1c3ac467902d2b83
Demi <furryalt68@gmail.com>
2022-05-18 22:56:28 +0200

    close #10; Commissione di 2 euro se totale inferiore a 10

--- src/test/java/it/unipd/mtss/business/BillImplTest.java
+++ src/test/java/it/unipd/mtss/business/BillImplTest.java
@@ -33,11 +33,11 @@ public class BillImplTest {
         
         itemsOrdered.add(new EItem( ItemType.Processor, "Pintel e700",110.00));
         itemsOrdered.add(new EItem( ItemType.Motherboard, "Azuz Pro", 60.00));
-        itemsOrdered.add(new EItem( ItemType.Mouse, "Fazer Neo", 30.00));
+        itemsOrdered.add(new EItem( ItemType.Mouse, "Fazer Neo", 5.00));
         itemsOrdered.add(new EItem( ItemType.Mouse, "Fazer Neo", 30.00)); // aggiungo altro mouse per non interferire con il test di MouseUgualiTastiere
         itemsOrdered.add(new EItem( ItemType.Keyboard, "Drevo Key", 45.00));
         
-        assertEquals(275, testBill.getOrderPrice(itemsOrdered,user), 0.0);
+        assertEquals(250, testBill.getOrderPrice(itemsOrdered,user), 0.0);
     }
     
     @Test(expected=BillException.class)
@@ -98,5 +98,11 @@ public class BillImplTest {
         }
         
         testBill.getOrderPrice(itemsOrdered, user);
+    }
+
+    @Test
+    public void testCommissione2EuroSeInferiore10Euro(){
+        itemsOrdered.add(new EItem( ItemType.Mouse, "Fazer Neo",5.00)); 
+        assertEquals(7.00, testBill.getOrderPrice(itemsOrdered,user), 0.0);
     }
 }
